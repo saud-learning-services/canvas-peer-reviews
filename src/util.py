49b2595154100f791a45677b3350d96a20d31719
Marko Prodanovic <marko.prodanovic@outlook.com>
2020-01-28 12:09:39 -0800

    This addresses #1 (key error: points). Workflow_state field from Canvas now appears in assessments table as State. It will indicate wheter a review has been completed or assigned (according to Canvas). When an assessment is completed but missing 'points' key, we ignore that review for our output and print a message to Jupyter notifying the user that a student may have inputted incorrect values into the rubric (The fact that you can do this is a Canvas bug).

--- src/util.py
+++ src/util.py
@@ -12,6 +12,15 @@ from termcolor import cprint
 import sys
 
 
+def print_error(msg):
+    """ Prints the error message without shutting down the script
+
+    Args:
+        msg (string): Message to print before continuing execution
+    """
+    cprint(f'\n{msg}\n', 'red')
+
+
 def shut_down(msg):
     """ Shuts down the script.
 
